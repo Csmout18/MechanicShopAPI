47a278b9abf0d855fd538236e5648654d21a7d84
Smout <smout18@gmail.com>
2026-03-20 16:32:23 -0500

    Reinstalled the Venv to fix interpreter issues

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\smout\OneDrive\Desktop\Coding Temple\Backend Module\LibraryAPI\venv
+set VIRTUAL_ENV=C:\Users\smout\OneDrive\Desktop\Coding Temple\Backend Module\MechanicShopAPI\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
